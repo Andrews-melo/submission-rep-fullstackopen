c16659181dd7a7523c4b69d0a79dc8cbd589e870
Andrews Melo <aam@cesar.org.br>
2026-04-07 19:46:42 -0300

    2.10: The Phonebook Step 5

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,35 @@
 import { useState } from 'react'
 
+const Filter = (props) => {
+  const { newFilter, handleFilterChange } = props
+  return (
+    <div>filter shown with: <input value={newFilter} onChange={handleFilterChange} /></div>
+  )
+}
+
+const PersonForm = (props) => {
+  const { addPerson, newName, handleNameChange, newNumber, handleNumberChange } = props
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = (props) => {
+  const { personToShow } = props
+  return (
+    personToShow.map(person => <div key={person.name}>{person.name} {person.number}</div>)
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState(
     [
@@ -53,20 +83,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with: <input value={newFilter} onChange={handleFilterChange} /></div>
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
       <h2>Add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
-
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      {personToShow.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      <Persons personToShow={personToShow} />
     </div>
   )
 }
