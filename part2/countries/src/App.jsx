16681658e93012d5bb7a7fecfc9850fafba2f770
Andrews Melo <aam@cesar.org.br>
2026-05-09 14:41:16 -0300

    2.18* Data for countries, step 1

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+const Country = ({ country }) => {
+  return (
+    <div>
+      <p>{country.name.common}</p>
+      <p>Capital {country.capital[0]}</p>
+      <p>Area {country.area}</p>
+      <p>Languages</p>
+      <ul>
+        {Object.values(country.languages).map(language => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} alt={country.flags.alt} />
+    </div>
+  )
+}
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    if (searchTerm.length > 0) {
+      axios.get(`https://restcountries.com/v3.1/name/${searchTerm}`).then((res) => {
+        setCountries(res.data)
+      })
+    } else {
+      setCountries([])
+    }
+  }, [searchTerm])
+
+
+  const handleCountryChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  return (
+    <>
+      <div>
+        <label>find countries</label>
+        <input type="text" onChange={handleCountryChange} value={searchTerm} />
+      </div>
+      <div>
+        {countries.length === 1 ? <Country country={countries[0]} /> : countries.length > 10 ? 'Too many matches, specify another filter' :
+          countries.map(country => (
+            <div key={country.cca3}>
+              <p>{country.name.common}</p>
+            </div>
+          ))}
+      </div>
+    </>
+  )
+}
+
+export default App
